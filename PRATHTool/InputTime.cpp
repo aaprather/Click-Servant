d145e06bbd4ca5f83f611cc18ce650ea220f5fa6
Aaron <34904535+aaprather@users.noreply.github.com>
2019-03-29 10:21:47 -0400

    built InputTime class

--- PRATHTool/InputTime.cpp
+++ PRATHTool/InputTime.cpp
@@ -1,7 +1,24 @@
 #include "InputTime.h"
 
 
-
-InputTime::InputTime()
+InputTime::InputTime(std::string h, std::string m, std::string s)
 {
+	Hours = parseText(h);
+	Minutes = parseText(m);
+	Seconds = parseText(s);
+}
+
+int InputTime::parseText(std::string text)
+{
+	int ret;
+	try
+	{
+		ret = std::stoi(text);
+	}
+	catch (...)
+	{
+		ret = 0;
+	}
+
+	return ret;
 }
