994ea0649f34538993333fafa28f63a3d0bd81a1
Aaron <34904535+aaprather@users.noreply.github.com>
2019-07-16 21:44:00 -0400

    Adjust draw position of Donate.h on button click.

--- Click-Servant/MyForm.h
+++ Click-Servant/MyForm.h
@@ -1360,7 +1360,7 @@ namespace PRATHTool {
 		MessageBox::Show("No updates found", "Checking for updates...");
 	}
 	private: System::Void ToolStripButton2_Click(System::Object^ sender, System::EventArgs^ e) {
-		Donate DN(1, System::Drawing::Point(this->Location.X + 145, this->Location.Y));
+		Donate DN(1, System::Drawing::Point(this->Location.X + 25, this->Location.Y));
 		MyForm::Hide();
 		DN.ShowDialog();
 		MyForm::Show();
